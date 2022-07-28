cc7a8d41b7c9f7d45f5c707f56418e60aed779fc
Ericka Jane Macatol <93069186+erickajanecm@users.noreply.github.com>
2022-07-28 19:34:41 +0800

    Committed on new branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,38 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'appointments',
+    loadChildren: () => import('./pages/appointments/appointments.module').then( m => m.AppointmentsPageModule)
+  },
+  {
+    path: 'homepage',
+    loadChildren: () => import('./pages/homepage/homepage.module').then( m => m.HomepagePageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,7 +6,37 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children:[
+      {
+        path:'homepage',
+        children:[
+          {
+            path:'',
+            loadChildren: () => import('../pages/homepage/homepage.module').then( m => m.HomepagePageModule)
+          }
+        ]
+      },
+      {
+        path:'profile',
+        children:[
+          {
+            path:'',
+            loadChildren: () => import('../pages/profile/profile.module').then( m => m.ProfilePageModule)
+          }
+        ]
+      },
+      {
+        path:'appointments',
+        children:[
+          {
+            path:'',
+            loadChildren: () => import('../pages/appointments/appointments.module').then( m => m.AppointmentsPageModule)
+          }
+        ]
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
